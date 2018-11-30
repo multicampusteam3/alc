2e41165ee144cf966152bd561ad69745d567e10c
dongjin <simpleicon0404@gmail.com>
2018-11-30 16:52:22 +0900

    Spring 2018/11/30 16:52

---
+++ src/com/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	
+	@RequestMapping("/main.alc")
+	public String main() {
+		return "main";
+	}
+
+	
+}
